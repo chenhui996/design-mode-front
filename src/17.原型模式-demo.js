ffdc425e4668c8df8a7fae725e5722934aef0670
cain <chen562450669@qq.com>
2021-05-30 16:19:18 +0800

    feat: 原型模式

---
+++ src/17.原型模式-demo.js
@@ -0,0 +1,25 @@
+// 原型模式-demo
+let prototype = {
+  getName: function () {
+    return this.first + '' + this.last
+  },
+  say: () => {
+    console.log('hello')
+  },
+}
+
+// 测试用例
+
+// 基于原型创建 objA
+let objA = Object.create(prototype)
+objA.first = 'A'
+objA.last = 'B'
+console.log(objA.getName())
+objA.say()
+
+// 基于原型创建 objB
+let objB = Object.create(prototype)
+objB.first = 'C'
+objB.last = 'D'
+console.log(objB.getName())
+objB.say()
