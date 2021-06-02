dc0a66cfd4e41611460d3ca4e6e382de3355d1de
cain <chen562450669@qq.com>
2021-06-02 15:32:35 +0800

    feat: 职责链模式

---
+++ src/23.职责链模式-demo.js
@@ -0,0 +1,25 @@
+// 职责链模式 - demo
+class Action {
+  constructor(name) {
+    this.name = name
+    this.nextAction = null
+  }
+  setNextAction(action) {
+    this.nextAction = action
+  }
+  handle() {
+    console.log(`${this.name} 审批`)
+    if (this.nextAction !== null) {
+      this.nextAction.handle()
+    }
+  }
+}
+
+// 测试用例
+let a1 = new Action('组长')
+let a2 = new Action('经理')
+let a3 = new Action('总监')
+
+a1.setNextAction(a2)
+a2.setNextAction(a3)
+a1.handle()
